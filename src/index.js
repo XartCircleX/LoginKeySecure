72cc75427ad8968405782db87467251b412d1c52
Javier Dominguez <alonsocorral2004@gmail.com>
2025-03-27 14:21:29 -0600

    Base CSS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const globalConstants = require('./const/globalConstants');
 const routerController = require('./routes/index.routes');
+const path = require('path');
 
 //--------------------------------------------
 const apiConfig = (app) => { //API configuration
@@ -14,12 +15,35 @@ const routerConfig = (app) => {
 };
 
 const init = () => {
+//-----------------------------------
+//Server
     const app = express(); //Express instance
 
     apiConfig(app); //API configuration
     routerConfig(app);
 
-    const usersPort = process.env.USERSPORT || 5000;
+//-----------------------------------
+//Static files config
+    // Use static archives form the folder "public"
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    //Redirect to login
+    app.get('/', (req, res) =>{
+        res.redirect('/login');
+    });
+
+    // Route for the login.html
+    app.get('/login', (req, res) => {
+        res.sendFile(path.join(__dirname, 'views', 'login.html'));
+    });
+
+    // Route for the login.html
+    app.get('/register', (req, res) => {
+        res.sendFile(path.join(__dirname, 'views', 'register.html'));
+    });
+//-----------------------------------
+
+    const usersPort = globalConstants.USERSPORT;
     //Confirmation that the server is running
     app.listen(usersPort, () => {
         console.log('\n\n--------------------------------------------');
